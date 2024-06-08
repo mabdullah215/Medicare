ac300fd69a198f48ceb4192323eb4e580e869ef9
Syed <mrabdullah1497@gmail.com>
2024-06-09 02:01:54 +0500

    UI elements added

--- app/src/main/java/com/syedapps/medicare/LoginActivity.java
+++ app/src/main/java/com/syedapps/medicare/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.syedapps.medicare;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+import com.dd.CircularProgressButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,6 +15,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        CircularProgressButton loginBtn=findViewById(R.id.button_progress);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(getBaseContext(),MainActivity.class));
+                Animatoo.INSTANCE.animateSlideLeft(LoginActivity.this);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/syedapps/medicare/fragments/AppointmentFragment.java
@@ -0,0 +1,21 @@
+package com.syedapps.medicare.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.syedapps.medicare.R;
+
+public class AppointmentFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_appointment, container, false);
+    }
+}
